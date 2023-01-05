bc6f75e96bd5c28bc0e7ebedfb3c4b11d5fb3d28
uiido <uiido@yahoo.com>
2023-01-05 13:34:09 -0500

    server

--- server.js
+++ server.js
@@ -3,23 +3,12 @@ const PORT = process.env.PORT || 3001;
 
 // Sets requirements
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+
 const app = express();
-const fs = require('fs');
-const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-// App.use functionality
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.use(express.static('public'));
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-// App listener
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.get('/', (req, res) => {
+    res.send('Note Taker');
 });
+
+app.listen(PORT);
