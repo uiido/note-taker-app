85d2ac97362209d5e7703c1a62536514ac8db4bb
uiido <uiido@yahoo.com>
2023-01-05 19:46:11 -0500

    Small server changes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// Variables
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Note Taker');
 });
 
-app.listen(PORT);
+// Listener
+app.listen(PORT, () => {
+    console.log(`This app is now ready to use at http://localhost:${PORT}!`);
+})
