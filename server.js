984971e275636e29dd5891c30c6499180bd17ee8
uiido <uiido@yahoo.com>
2023-01-05 19:52:55 -0500

    Added middleware/small changes to server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.get('/', (req, res) => {
     res.send('Note Taker');
 });
 
+// Middleware for JSON and URL Encoded
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Listener
 app.listen(PORT, () => {
-    console.log(`This app is now ready to use at http://localhost:${PORT}!`);
+    console.log(`This app is now ready to use at http://localhost:${PORT} !`);
 })
